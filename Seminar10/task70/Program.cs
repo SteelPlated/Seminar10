73d286d7624513ec07bc745e519c45d8edcadf32
SteelPlated <mix.singatulin@yandex.ru>
2022-06-13 22:02:23 +0500

    1

---
+++ Seminar10/task70/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 70: Напишите программу, которая на вход принимает два числа и выдаёт
+первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+3 и 4, N = 5 -> 3 4 7 11 18
+6 и 10, N = 4 -> 6 10 16 26*/
+
+Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+void GetProgSumm(int num1, int num2, int count)
+{
+    int sum=0;
+    if(count==2)return;
+    sum+=num1+num2;
+    num1=num2;
+    num2=sum;
+    Console.Write($"{sum} ");
+    GetProgSumm(num1, num2, count-1);
+}
+Console.Write($"{a},{b} ");
+GetProgSumm(a,b,n);
---
+++ Seminar10/task71/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 71: В некотором машинном алфавите имеются четыре буквы «а», «и», «с»
+и «в». Покажите все слова, состоящие из n букв, которые можно построить из
+букв этого алфавита.
+n = 2 -> аа, ии, сс, вв, аи, иа, ис, си, ас, са,
+ав, ва, ви, ив, св, вс*/
+
+string charsWords = "аисв";
+int countCharsInWords = 5;
+
+PrintAllWords(charsWords, countCharsInWords, "");
+
+void PrintAllWords(string alphabet, int length, string prefix)
+{
+    if (length == 0) Console.Write(prefix + " ");
+    else foreach (char c in alphabet) PrintAllWords(alphabet, length - 1, prefix + c);
+}
